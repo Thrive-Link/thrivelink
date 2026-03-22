3197890c82e8f887689c0a9ec46078618caef00f
oluwaseyipd <oluwaseyipd@gmail.com>
2026-03-22 14:40:41 +0100

    feat: Add Events and Programs pages with SectionHero component

---
+++ app/events/page.tsx
@@ -0,0 +1,10 @@
+import SectionHero from "@/components/SectionHero";
+
+
+export default function Events() {
+  return (
+    <div>
+      <SectionHero title="Events" description="Join us for upcoming events and activities." />
+    </div>
+  )
+}
---
+++ app/programs/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/Footer";
+import SectionHero from "@/components/SectionHero";
+
+
+export default function Programs() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <SectionHero title="Our Programs" description="Explore the various programs we offer to support your journey." />
+   
+   
+   
+    {/* Footer */}
+    <Footer />
+    </div>
+  )
+}
